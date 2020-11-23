fe05be83296d2ac04d3ce9adb12964b2585b3766
lmis <lionel.miserez@gmail.com>
2020-11-23 19:01:23 +0100

    Make rendering scale invariant

---
+++ src/config.ts
@@ -0,0 +1,28 @@
+import { MovementConfig } from "physics";
+
+export const signalingUrl = "https://rgtx5.sse.codesandbox.io/";
+
+export const gameBorders = {
+  bottom: 0,
+  top: 800,
+  left: 0,
+  right: 800
+};
+
+export const canvasWidth = 800;
+export const canvasHeight = 800;
+export const playerRadius = 75;
+export const wallThickness = 5;
+
+export const movementConfig: MovementConfig = {
+  delta: 0.05,
+  friction: 0.2,
+  start: [430, 150],
+  constraint: ([x, y]) =>
+    x >= gameBorders.left + playerRadius + wallThickness &&
+    x <= gameBorders.right - playerRadius - wallThickness &&
+    y >= gameBorders.bottom + playerRadius + wallThickness &&
+    y <= gameBorders.top - playerRadius - wallThickness
+};
+
+export const thrust = 900;
